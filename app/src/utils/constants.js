09e582c4a166639e3a42c8c6e08c3e2387a8755d
Andrew MacPherson <andrewmac17@gmail.com>
2018-05-12 13:58:48 -0400

    config urls

--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -1,9 +1,9 @@
 const APP_ENV = process.env.REACT_APP_ENV;
 
 if(APP_ENV === 'development') {
-    var URL          = 'http://localhost:3000';
-    var API_BASE_URL = 'http://localhost:3001';
-    var UPLOADS_URL = 'http://localhost:8888/dragonboatlife/app';
+    var URL          = 'http://app.dragonboatlife.com';
+    var API_BASE_URL = 'http://app.dragonboatlife.com:3001';
+    var UPLOADS_URL = 'http://app.dragonboatlife.com/app';
 } else if(APP_ENV === 'production') {
     var URL          = 'http://app.dragonboatlife.com';
     var API_BASE_URL = 'http://app.dragonboatlife.com:3001';
